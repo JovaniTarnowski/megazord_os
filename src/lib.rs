f7eab6d9c791dac92582148c6de75bc9df8d95d5
Jovani <jovanitarnowski@gmail.com>
2021-05-30 20:09:53 -0300

    Added custom paging, instead of using the bootloader one

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,13 @@ pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod memory;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
 
 pub fn init() {
     gdt::init();
@@ -72,8 +79,7 @@ pub fn hlt_loop() -> ! {
 
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
